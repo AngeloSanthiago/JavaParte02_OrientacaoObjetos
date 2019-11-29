dfc2dbfd97d077bc1261067791c7f39dd2019e56
angeloNotebook <angelosanthiago@gmail.com>
2019-11-29 20:56:04 -0300

    Cap05 - implementação completa

--- bytebank-encapsulado/src/br/com/home/TestaGetESet.java
+++ bytebank-encapsulado/src/br/com/home/TestaGetESet.java
@@ -6,5 +6,20 @@ public class TestaGetESet {
 //		conta.numero = -1337; Não compila
 		conta.setNumero(1337);
 		System.out.println(conta.getNumero());
+		
+		Cliente paulo = new Cliente();
+		paulo.setNome("Paulo Silveira");
+		conta.setTitular(paulo);
+		System.out.println(conta.getTitular());
+		System.out.println(conta.getTitular().getNome());
+		
+		conta.getTitular().setProfissao("Programador");
+		
+		Cliente titularDaConta = conta.getTitular();
+		titularDaConta.setProfissao("Programador");
+		
+		System.out.println(titularDaConta );
+		System.out.println(paulo);
+		System.out.println(conta.getTitular());
 	}
 }
